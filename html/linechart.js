24a94dc9499fcc85819712c17da294ebe691a1ad
Henry <16773293@qq.com>
2018-03-03 01:22:19 +0800

    linechart

--- html/linechart.js
+++ html/linechart.js
@@ -10,61 +10,67 @@ function randomData() {
     }
 }
 
-var data = [];
+function formChartData(data) {
+    now = new Date(+now + oneDay);
+    value = value + Math.random() * 21 - 10;
+    return {
+        name: now.toString(),
+        value: [
+            [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+            data["buy_price"]
+        ]
+    }
+}
+
 var now = +new Date(1997, 9, 3);
 var oneDay = 24 * 3600 * 1000;
 var value = Math.random() * 1000;
-for (var i = 0; i < 1000; i++) {
-    data.push(randomData());
-}
+//for (var i = 0; i < 1000; i++) {
+//    data.push(randomData());
+//}
 
-option = {
-    title: {
-        text: '动态数据 + 时间坐标轴'
-    },
-    tooltip: {
-        trigger: 'axis',
-        formatter: function (params) {
-            params = params[0];
-            var date = new Date(params.name);
-            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
-        },
-        axisPointer: {
-            animation: false
-        }
-    },
-    xAxis: {
-        type: 'time',
-        splitLine: {
-            show: false
-        }
-    },
-    yAxis: {
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-            show: false
-        }
-    },
-    series: [{
-        name: '模拟数据',
-        type: 'line',
-        showSymbol: false,
-        hoverAnimation: false,
-        data: data
-    }]
+var linechart = {
+  data : [],
+  option : {
+      title: {
+          text: '动态数据 + 时间坐标轴'
+      },
+      tooltip: {
+          trigger: 'axis',
+          formatter: function (params) {
+              params = params[0];
+              var date = new Date(params.name);
+              return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+          },
+          axisPointer: {
+              animation: false
+          }
+      },
+      xAxis: {
+          type: 'time',
+          splitLine: {
+              show: true
+          }
+      },
+      yAxis: {
+          type: 'value',
+          boundaryGap: [0, '100%'],
+          splitLine: {
+              show: true
+          }
+      },
+      series: [{
+          name: '模拟数据',
+          type: 'line',
+          showSymbol: false,
+          hoverAnimation: false,
+          data: this.data
+      }]
+  },
+  myChart:{},
+  init:function(domchart){
+    this.myChart = echarts.init(domchart);
+    this.myChart.setOption(this.option);
+    return this;
+  },
 };
-
-setInterval(function () {
-
-    for (var i = 0; i < 5; i++) {
-        data.shift();
-        data.push(randomData());
-    }
-
-    myChart.setOption({
-        series: [{
-            data: data
-        }]
-    });
-}, 1000);
