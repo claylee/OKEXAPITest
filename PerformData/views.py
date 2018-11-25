54cc05cb14b2a2171a611f2fa105a06f58fa3866
claylee <16773293@qq.com>
2018-11-25 15:37:04 +0800

    save ticker to db

--- PerformData/views.py
+++ PerformData/views.py
@@ -1,8 +1,9 @@
-from stageData import *
+from stageData import dataSchema
 from . import performData
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from pythonReq import okcoinRest
+TradePrice = dataSchema.TradePrice
 
 @performData.route("/",methods=["GET","POST"])
 def index():
@@ -13,3 +14,9 @@ def index():
 def showOKEx():
     content = okcoinRest.realTrades()
     return render_template("performData/OKEx.html",tradeContent)
+
+
+@performData.route("/tickers",methods=["GET","POST"])
+def tickers():
+    tickList = TradePrice.query.all()
+    return render_template("performData/tickers.html",tickers = tickList)
